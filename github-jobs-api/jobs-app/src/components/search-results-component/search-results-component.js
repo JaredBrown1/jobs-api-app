4afbb823b9084fe37bcae7fd9e8a371143013246
Jared Brown <jaredbrown1@hotmail.com>
2021-01-03 14:23:36 -0800

    messed with api

--- github-jobs-api/jobs-app/src/components/search-results-component/search-results-component.js
+++ github-jobs-api/jobs-app/src/components/search-results-component/search-results-component.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchResults = () => {
+	const [jobs, setJobs] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:8010/proxy")
+			.then((response) => {
+				setJobs(response.data);
+				console.log("Returned data", response.data);
+			})
+			.catch((error) => {
+				console.log("The data was not returned", error);
+			});
+	}, []);
+
 	return <h1>Search-results component</h1>;
 };
 
--- github-jobs-api/jobs-app/src/components/title-component/title-component.js
+++ github-jobs-api/jobs-app/src/components/title-component/title-component.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Button from "./button";
-
 import { Container } from "./title-styles";
 
 const Title = () => {
